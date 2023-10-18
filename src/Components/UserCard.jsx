78379746c2d7a810fbfd07848d442443230facf3
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2023-10-18 14:36:37 +0530

    about data feched successfullly

--- src/Components/UserCard.jsx
+++ src/Components/UserCard.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../style/About.css';
 
 const UserCard = ({name,location}) => {
+
+  const [userData, setUserData] = useState({
+    name:"",
+    avatar_url:"",
+    location:"",
+    bio:""
+  })
+
+  async function fetchData(){
+    try {
+      const response = await fetch('https://api.github.com/users/Nitishcoderr')
+      const data = await response.json()
+      console.log(data);
+      setUserData(data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  
+
+
   return (
     <div className='user-card'>
-      <h1>Name - {name}</h1>
-      <h3>Location - {location}</h3>
-      <h4>Contact - @gmail.com</h4>
+      <img src={userData.avatar_url} alt="avatar" />
+      <h1>Name - {userData.name}</h1>
+      <h3>Location - {userData.location}</h3>
+      <h4>Bio - {userData.bio}</h4>
     </div>
   )
 }
